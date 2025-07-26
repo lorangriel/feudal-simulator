94b7163f0dd8d1b470aa70e949d5c462eddb5582
Anders Hedström <97519292+lorangriel@users.noreply.github.com>
2025-07-26 12:13:29 +0200

    Link SDL2 (#112)
    Link SDL2 library

--- src2/Main.cpp
+++ src2/Main.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
-
-
+#include <SDL2/SDL.h>
 
 int main(int argc, char *argv[])
 {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to init SDL: %s", SDL_GetError());
+        return 1;
+    }
+    SDL_Quit();
+    return 0;
 }
 
